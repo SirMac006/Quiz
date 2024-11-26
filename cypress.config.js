498069fa27e35928a5cf577940adf1245ead2e47
SirMac <sir.mattew.holmes@gmail.com>
2024-11-25 23:28:22 -0700

    working on debuggin

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  baseUrl: "http://localhost:3000",
+  component: {
+    componentFolder: "src/components",
+    testFiles: "**/*.spec.js"
+  },
   e2e: {
+    supportFile: "cypress/support/index.js",
+    fixturesFolder: "cypress/fixtures",
+    integrationFolder: "cypress/integration",
+    pluginsFile: "cypress/plugins/index.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/component/QuizComponent.spec.js
+++ cypress/component/QuizComponent.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'cypress/react';
-import QuizComponent from '../../src/components/QuizComponent';
+import QuizComponent from '../../../src/components/QuizComponent';
 
 describe('QuizComponent', () => {
   it('renders the quiz component', () => {
